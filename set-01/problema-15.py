1d262f36b81c365cb925bda5321567c81936082a
Paul Filip <user1910524aaaa@gmail.com>
2022-11-18 10:36:18 +0200

    replaced sorted function with bubble sort algorithm

--- set-01/problema-15.py
+++ set-01/problema-15.py
@@ -19,7 +19,7 @@ cuvantul2_sortat_crescator = [x for x in cuvantul2]
 # verificam daca ambele cuvinte au acelasi numar de caractere
 if(len(cuvantul1) == len(cuvantul2)):
     
-    # ordonam crescator caracterele primului cuvant cu ajutorul algoritmului bubble sort
+    # ordonam in ordine alfabetica caracterele primului cuvant cu ajutorul algoritmului bubble sort
     # cuvantul1_sortat_crescator = sorted(cuvantul1)
     temp = ''
     for i in range(0, len(cuvantul1)):
@@ -29,7 +29,7 @@ if(len(cuvantul1) == len(cuvantul2)):
                 cuvantul1_sortat_crescator[i] = cuvantul1_sortat_crescator[j]
                 cuvantul1_sortat_crescator[j] = temp
     
-    # ordonam crescator caracterele celui de-al doilea cuvant cu ajutorul algoritmului bubble sort
+    # ordonam in ordine alfabetica caracterele celui de-al doilea cuvant cu ajutorul algoritmului bubble sort
     # cuvantul2_sortat_crescator = sorted(cuvantul2)            
     temp = ''
     for i in range(0, len(cuvantul2)):
